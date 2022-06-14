4fbb54b8335ffebda5b9641a4373143907b81d16
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-06-14 13:44:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ server.listen(port, () => {
 });
 
 app.use(siofu.router);
-
-// Routing
 app.use(express.static(path.join(__dirname, 'public')));
-var playing = false;
 var files = [];
 var playlist = groove.createPlaylist();
 var player = groove.createPlayer();
@@ -25,58 +22,66 @@ var devices = groove.getDevices();
 var defaultDevice = devices.list[devices.defaultIndex];
 player.device = defaultDevice;
 player.attach(playlist,(err)=>{if (err) throw err});
-function updatePacket() {
-  let current = player.position();
+function playlistPacket() {
   let l = [];
+  for (;playlist.count()&&(player.position().item!=playlist.items()[0]);) {
+    let item = playlist.items()[0];
+    let file = item.file;
+    playlist.remove(item);
+    file.close();
+  }
+  let current = player.position();
   for (let item of playlist.items()) {
     let file = item.file;
     let meta = file.metadata();
     meta.filename = file.filename;
     meta.duration = file.duration();
-    l.push(meta)
+    l.push(meta);
   }
-  let meta = {title:"Playlist Empty"}
+  let meta = null;
   if (current.item) {
-    let meta = 
-  let file = current.item.file;
-  let meta = file.metadata();
-  meta.filename = file.filename;
-  meta.duration = file.duration;
-  meta.position = current.pos;
+    let file = current.item.file;
+    let meta = file.metadata();
+    meta.filename = file.filename;
+    meta.duration = file.duration;
+    meta.position = current.pos;
+  }
   return {nowPlaying: meta, playlist: l, playing: player.playing()};
+}
+function timePacket() {
+  return {playing: playlist.playing(), position: (player.position().pos)||0};
 }
 io.on('connection', (socket) => {
   player.on('nowPlaying',()=>{
     let current = player.position();
     if (!current.item) {
-      playlist.clear()
-      for (let file of files) {
-        file.close()
-      }
+      playlist.seek()
     }
-    socket.broadcast.emit('update',updatePacket())
+    socket.broadcast.emit('playlist', playlistPacket());
   });
   var uploader = new siofu();
   uploader.dir = "/app/audio";
   uploader.listen(socket);
   uploader.on('saved', (event) => {
-    groove.open(event.file,(err,file) => {
+    groove.open(event.file.name,(err,file) => {
       if (err) {console.log(err); return null;}
-      files.push(file);
       playlist.insert(file);
-      let l = []
-      for (let item of playlist.items()) {
-        l.push([item.file.filename,item.file.metadata()])
-      }
-      socket.broadcast.emit('updateList',{});
+      socket.broadcast.emit('playlist', playlistPacket());
     });
   })
   socket.on('play', (data) => {
-    if (!playlist.playing) {
+    if (!playlist.playing()) {
       playlist.play();
     }
-    socket.broadcast.emit('play', {
-      
-    });
+    socket.broadcast.emit('play', timePacket());
+  });
+  socket.on('pause', (data) => {
+    if (playlist.playing()) {
+      playlist.pause();
+    }
+    socket.broadcast.emit('pause', timePacket())
+  });
+  socket.on('query', (data) => {
+    socket.broadcast.emit('playlist', playlistPacket());
   });
 });
